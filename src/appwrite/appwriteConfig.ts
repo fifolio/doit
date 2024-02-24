d272b72a7fc3a58f8352cb724cf473b656cdb211
Firas <firasdabbabi@gmail.com>
2024-02-24 20:46:56 +0100

    set Signup form

--- src/appwrite/appwriteConfig.ts
+++ src/appwrite/appwriteConfig.ts
@@ -1,10 +1,11 @@
 import { Client, Account, Databases } from 'appwrite';
-import envs from '../vite-env';
 
 // Client Configuration
 const client = new Client();
-client.setEndpoint(`${envs.VITE_ENDPOINT_URL}`).setProject(`${envs.VITE_PROJECT_ID}`);
+
+client.setEndpoint(`${import.meta.env.VITE_ENDPOINT_URL}`).setProject(`${import.meta.env.VITE_PROJECT_ID}`);
+    
 export const account = new Account(client);
 
 // Configure and refer to the Databases
-export const databases = new Databases(client, `${envs.VITE_DATABASES_ID}`)
+export const databases = new Databases(client);
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,10 @@
+import { ID } from 'appwrite'
 
 export default function login() {
+
+  console.log('the id:', ID.unique());
+
+
   return (
     <div>login</div>
   )
